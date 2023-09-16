b0c6ca43fdd794fadd004a02a5e47f4092f39b4c
zhaopeng <zhaopeng_eng@163.com>
2023-09-16 09:13:04 +0000

    [script] add run scripts
    Signed-off-by: zhaopeng <zhaopeng_eng@163.com>

---
+++ tensorrt_llm_july-release-v1/examples/camel5b/run.sh
@@ -0,0 +1,3 @@
+python run.py \
+--engine_dir=/root/workspace/hackatnon_final/tensorrt_llm_july-release-v1/examples/camel5b/gpt_outputs_org/ \
+--max_output_len=256
---
+++ tensorrt_llm_july-release-v1/examples/camel5b/summarize.sh
@@ -0,0 +1,3 @@
+python summarize.py \
+--hf_model_location=/root/workspace/camel_5b/camel-5b-hf/ --data_type=fp16 \
+--engine_dir=/root/workspace/hackatnon_final/tensorrt_llm_july-release-v1/examples/camel5b/gpt_outputs_org/
